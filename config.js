7d2af14d5c2e87da37de71c7601a4d5fe86d9e97
gaspar-cloud <gasparperelloalvear@gmail.com>
2025-09-19 03:11:51 -0300

    fenix config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-const GH_CONFIG = {
-  usuario: 'gaspar-cloud',
-  repo: 'gaspi-s-little-liminal-space',
-  token: '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'
+const CLOUDINARY_CONFIG = {
+  cloudName: 'dc9dths1n', // Reemplaza con tu Cloud Name de Cloudinary
+  uploadPreset: 'Gaspi's little liminal space storage' // Reemplaza con el nombre de tu preset sin firmar
 };
