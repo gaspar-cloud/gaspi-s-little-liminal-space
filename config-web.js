beba78e8b1674816fa2649489f9f91ca3f4f68e6
gaspar-cloud <gasparperelloalvear@gmail.com>
2025-09-19 12:23:39 -0300

    Update config-web.js

--- config-web.js
+++ config-web.js
@@ -1,4 +1,4 @@
 const CLOUDINARY_CONFIG = {
-  cloudName: 'dc9dths1n', // Reemplaza con tu Cloud Name de Cloudinary
-  uploadPreset: 'Gaspi's little liminal space storage' // Reemplaza con el nombre de tu preset sin firmar
-};
+  cloudName: 'dc9dths1n',
+  uploadPreset: "Gaspi's little liminal space storage" 
+}; 
