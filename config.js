6cdbb13e7f9849c21d4c7ceb433d1ebe2a876391
gaspar-cloud <gasparperelloalvear@gmail.com>
2025-09-19 00:46:16 -0300

    (2) config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
-// config.js - NO SUBIR ESTE ARCHIVO A GITHUB
-// Añade config.js a .gitignore
 const GH_CONFIG = {
   usuario: 'gaspar-cloud',
   repo: 'gaspi-s-little-liminal-space',
-  token: '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' // Reemplazar con tu token
+  token: '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'
 };
+
